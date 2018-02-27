eee25d4650a38666c17ab0d516384a5eb397fd6f
Kirk <kmadera@robofirm.com>
2018-02-27 12:56:21 -0600

    Reduced nice setting on commands to -19 because -20 requires root access

--- src/Shell/Adapter/SshAdapter.php
+++ src/Shell/Adapter/SshAdapter.php
@@ -114,7 +114,7 @@ class SshAdapter implements ShellAdapterInterface, LoggerAwareInterface
         if (shellAdapterInterface::PRIORITY_LOW == $priority) {
             $command = 'ionice -c3 nice -n 19 bash -c ' . escapeshellarg($command);
         } elseif (shellAdapterInterface::PRIORITY_HIGH == $priority) {
-            $command = 'nice -n -20 bash -c ' . escapeshellarg($command);
+            $command = 'nice -n -19 bash -c ' . escapeshellarg($command);
             if (0 == posix_getuid()) {
                 $command = 'ionice -c 1 -n 0 ' . $command;
             } else {
